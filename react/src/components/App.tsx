763c576adb9219c81ac1e9963dd82b2842259f4d
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2021-09-25 17:34:48 -0500

    New configuration WASM

--- react/src/components/App.tsx
+++ react/src/components/App.tsx
@@ -4,11 +4,9 @@ import Game from "./Game"
 import Header from "./Header"
 
 const App = () => {
-  import("algorithm").then(({ add_two_ints, multiply_two_ints }) => {
+  import("langtons-ant-algorithm").then(({ add_two_ints }) => {
     const sumResult = add_two_ints(100, 20)
-    const mulResult = multiply_two_ints(5, 5)
     console.log("Sum", sumResult)
-    console.log("Mul", mulResult)
   })
 
   return (
