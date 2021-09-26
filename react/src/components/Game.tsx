d31be9e59558b039ae6c15b746ea40ca9447cc13
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2021-09-26 01:53:04 -0500

    Cell space

--- react/src/components/Game.tsx
+++ react/src/components/Game.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react"
 
-const canvasW = 300
-const canvasH = 300
-const cols = 15
-const rows = 15
+const width = 300
+const height = 300
+const cellSize = 5
+let cellSpace = cellSize + 1
+const cols = 10
+const rows = 10
 let canvas: HTMLCanvasElement
 let context: CanvasRenderingContext2D
 let board: Array<Array<Cell>>
-let cellH: number
-let cellW: number
 
 class Cell {
   x: number
@@ -23,7 +23,9 @@ class Cell {
     if (context !== null) {
       context.fillStyle = this.state === 1 ? "#E1EA7F" : "#45C53F"
       // fillRect(x, y, width, height)
-      context.fillRect(this.x * cellW, this.y * cellH, cellW, cellH)
+      const x = cellSpace * this.x + 1
+      const y = cellSpace * this.y + 1
+      context.fillRect(x, y, cellSize, cellSize)
     }
   }
 }
@@ -31,20 +33,14 @@ class Cell {
 const loadGame = () => {
   canvas = document.getElementById("canvas") as HTMLCanvasElement
   context = canvas.getContext("2d") as CanvasRenderingContext2D
-  canvas.width = canvasW
-  canvas.height = canvasH
-  cellW = Math.ceil(canvasW / cols)
-  cellH = Math.ceil(canvasH / rows)
+  canvas.width = cellSpace * width
+  canvas.height = cellSpace * height
   board = Array.from(new Array(cols)).map(row => new Array(rows)) as Array<Array<Cell>>
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
       // const state = Math.floor(Math.random() * 2)
       const state = y % 2 === 0 ? (x % 2 === 0 ? 0 : 1) : x % 2 === 0 ? 1 : 0
       board[x][y] = new Cell(x, y, state)
-    }
-  }
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
       board[x][y].paint()
     }
   }
